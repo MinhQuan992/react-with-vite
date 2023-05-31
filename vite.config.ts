62d44633c35517d192ca07db64f0c990988d6e98
Trần Minh Quân Võ <vtmquan0909@gmail.com>
2023-05-31 22:49:57 +0700

    try using some configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({command, mode, ssrBuild}) => {
+  console.log(command, mode, ssrBuild);
+
+  const env = loadEnv(mode, process.cwd(), "VITE")
+  console.log(env);
+
+  return {
+    plugins: [react()],
+    clearScreen: false,
+    server: {
+      port: 3000
+    },
+    preview: {
+      port: 8080
+    }
+  }
 })
